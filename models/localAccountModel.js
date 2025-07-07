70d5f315fbcaa55fe5073379cf79beee3db2714e
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-08 04:39:26 +0800

    feat: Create local account model

---
+++ models/localAccountModel.js
@@ -0,0 +1,66 @@
+const { PrismaClient } = require("../generated/prisma");
+const prisma = new PrismaClient();
+
+async function create(data) {
+	try {
+		const localAccount = await prisma.localAccount.create({
+			data,
+		});
+
+		return localAccount;
+	} catch (error) {
+		console.error("Failed to create the local account. ", error);
+		throw error;
+	}
+}
+
+async function findByOptions(options) {
+	try {
+		const localAccount = await prisma.localAccount.findUnique({
+			where: options,
+		});
+
+		return localAccount;
+	} catch (error) {
+		console.error("Failed to retrieve the local account. ", error);
+		throw error;
+	}
+}
+
+async function updateById({ id, data }) {
+	try {
+		const updatedLocalAccount = await prisma.localAccount.update({
+			where: {
+				id,
+			},
+			data: data,
+		});
+
+		return updatedLocalAccount;
+	} catch (error) {
+		console.error("Failed to update the local account. ", error);
+		throw error;
+	}
+}
+
+async function deleteById(id) {
+	try {
+		const deletedLocalAccount = await prisma.localAccount.delete({
+			where: {
+				id,
+			},
+		});
+
+		return deletedLocalAccount;
+	} catch (error) {
+		console.error("Failed to delete the local account. ", error);
+		throw error;
+	}
+}
+
+module.exports = {
+	create,
+	findByOptions,
+	updateById,
+	deleteById,
+};
