d60ce422068d124ccec8213b4f3cac7055ef7d62
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-08 06:45:17 +0800

    feat: Display email verification notice page on succesfull initial user registration

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,9 +1,24 @@
+const authServices = require("../services/authServices.js");
+
 async function renderSignUpPage(req, res, next) {
 	return res.status(200).render("signUp", {
 		formFieldData: null,
 	});
 }
 
+async function signUpUser(req, res, next) {
+	// Register the user
+	await authServices.registerLocalUser({
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password,
+	});
+
+	// Render sign up success messages
+	return res.status(200).render("emailVerificationNotice");
+}
+
 module.exports = {
 	renderSignUpPage,
+	signUpUser,
 };
