9ac789ab9b7615ca0960880e529be9f81efea2c1
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-08 04:38:07 +0800

    feat: Create user model

---
+++ models/userModel.js
@@ -0,0 +1,68 @@
+const { PrismaClient } = require("../generated/prisma");
+const prisma = new PrismaClient();
+
+async function create(data) {
+	try {
+		const user = await prisma.user.create({
+			data,
+		});
+
+		return user;
+	} catch (error) {
+		console.error("Failed to create the user. ", error);
+		throw error;
+	}
+}
+
+async function findByOptions(options) {
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				options,
+			},
+		});
+
+		return user;
+	} catch (error) {
+		console.error("Failed to retrieve the user. ", error);
+		throw error;
+	}
+}
+
+async function updateById({ id, data }) {
+	try {
+		const updatedUser = await prisma.user.update({
+			where: {
+				id,
+			},
+			data: data,
+		});
+
+		return updatedUser;
+	} catch (error) {
+		console.error("Failed to update the user. ", error);
+		throw error;
+	}
+}
+
+async function deleteById(id) {
+	try {
+		const deletedUser = await prisma.user.delete({
+			where: {
+				id,
+			},
+		});
+
+		return deletedUser;
+	} catch (error) {
+		console.error("Failed to delete the user. ", error);
+		throw error;
+	}
+}
+
+module.exports = {
+	create,
+	findByOptions,
+	updateById,
+	deleteById,
+};
