09763084c6e0465882a796666e3e44b5fe5066f7
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-08 12:52:15 +0800

    cleanup: Remove debugging line

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -99,7 +99,6 @@ async function signInUserLocally(req, res, next) {
 		} else {
 			// Render email-verification-sent page if the user signing-in is not yet verified
 			if (!user.localAccount.isVerified) {
-				console.log(user);
 				// Render email verification page
 				return res.status(200).render("emailVerificationNotice");
 			}
