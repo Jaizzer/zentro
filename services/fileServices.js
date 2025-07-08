9ac1981a045fd9b2e6e67a2d2fab83fe33989c5d
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-09 07:59:00 +0800

    feat: Create service for saving the files in the cloud and in the database

---
+++ services/fileServices.js
@@ -0,0 +1,46 @@
+// Load path to resolve the absolute path to the .env file (one level up from current directory)
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+// Load models
+const File = require("../models/fileModel.js");
+
+// Load services
+const storageServices = require("../services/storageServices.js");
+
+// Load utilities
+const generateRandomString = require("../utils/generateRandomString.js");
+
+async function save({ fileUploads, userId, folderId }) {
+	// Hash the filenames
+	fileUploads.forEach((fileUpload) => {
+		fileUpload.name = generateRandomString();
+	});
+
+	// Same the file uploads to the cloud in-parallel
+	await Promise.all(
+		fileUploads.map((fileUpload) => {
+			return storageServices.uploadFile({
+				file: fileUpload.file,
+				name: fileUpload.name,
+				type: fileUpload.type,
+			});
+		})
+	);
+
+	// Create the files' metadata
+	const metadata = fileUploads.map((fileUpload) => ({
+		name: fileUpload.name,
+		type: fileUpload.type,
+        size: fileUpload.size,
+		ownerId: userId,
+		folderId: folderId === "root" ? null : folderId,
+	}));
+
+	// Save the file metadata to the database
+	await File.createMany(metadata);
+}
+
+module.exports = {
+	save,
+};
