60f7205bace76dbc6c9ac5d3fbc10cb514b02c67
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-09 06:47:32 +0800

    ref: Remove file prefixes

--- services/storageServices.js
+++ services/storageServices.js
@@ -28,14 +28,14 @@ const cloudFront = new CloudFrontClient({
 	region: process.env.BUCKET_REGION,
 });
 
-async function uploadFile({ file, fileName, fileType }) {
+async function uploadFile({ file, name, type }) {
 	try {
 		// Upload file to S3
 		const params = {
 			Bucket: process.env.BUCKET_NAME,
-			Key: fileName,
+			Key: name,
 			Body: file,
-			ContentType: fileType,
+			ContentType: type,
 		};
 		const command = new PutObjectCommand(params);
 		await s3.send(command);
@@ -44,12 +44,12 @@ async function uploadFile({ file, fileName, fileType }) {
 	}
 }
 
-async function deleteFile(fileName) {
+async function deleteFile(name) {
 	try {
 		// Delete the file from S3
 		const params = {
 			Bucket: process.env.BUCKET_NAME,
-			Key: fileName,
+			Key: name,
 		};
 		const command = new DeleteObjectCommand(params);
 		await s3.send(command);
@@ -58,10 +58,10 @@ async function deleteFile(fileName) {
 		const invalidationParams = {
 			DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
 			InvalidationBatch: {
-				CallerReference: fileName,
+				CallerReference: name,
 				Paths: {
 					Quantity: 1,
-					Items: ["/" + fileName],
+					Items: ["/" + name],
 				},
 			},
 		};
@@ -74,11 +74,11 @@ async function deleteFile(fileName) {
 	}
 }
 
-async function getFileUrl(fileName) {
+async function getFileUrl(name) {
 	try {
 		// Get the cloudfront signed URL
 		fileUrl = getSignedUrl({
-			url: `${process.env.CLOUDFRONT_DOMAIN_NAME}/${fileName}`,
+			url: `${process.env.CLOUDFRONT_DOMAIN_NAME}/${name}`,
 			dateLessThan: new Date(Date.now() + 1000 * 60 * 60 * 24),
 			privateKey: process.env.CLOUDFRONT_PRIVATE_KEY,
 			keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
