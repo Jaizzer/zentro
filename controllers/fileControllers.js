c4fac7553ffe57ba3ba0efc15de55d40866b8289
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-09 08:00:40 +0800

    feat: Creat routes for uploading files

--- controllers/fileControllers.js
+++ controllers/fileControllers.js
@@ -1,12 +1,34 @@
 const asyncHandler = require("express-async-handler");
+const fileServices = require("../services/fileServices.js");
 
 async function renderFileUploadPage(req, res, next) {
 	return res.status(200).render("uploadFiles", {
 		formFieldData: null,
-		folderId: "",
+		folderId: "root",
 	});
+}
+
+async function uploadFiles(req, res, next) {
+	const folderId = req.params.folderId;
+
+	// Extract the only necessary file data
+	const mediaUploads = req.files.map((file) => ({
+		file: file.buffer,
+		name: file.originalname,
+		type: file.mimetype,
+		size: file.size,
+	}));
+
+	await fileServices.save({
+		userId: req.user.id,
+		fileUploads: mediaUploads,
+		folderId: folderId,
+	});
+
+	return res.status(302).redirect("/");
 }
 
 module.exports = {
 	renderFileUploadPage: asyncHandler(renderFileUploadPage),
+	uploadFiles: asyncHandler(uploadFiles),
 };
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,12 +1,24 @@
 const { Router } = require("express");
 const fileRouter = Router();
 const fileControllers = require("../controllers/fileControllers");
+const fileMiddlewares = require("../middlewares/fileMiddlewares.js");
+const fileValidators = require("../validators/fileValidators.js");
 const authMiddlewares = require("../middlewares/authMiddlewares.js");
+const fileUpload = require("../config/multer.js");
 
 fileRouter.get(
 	"/upload",
 	authMiddlewares.isAuthenticated,
 	fileControllers.renderFileUploadPage
+);
+
+fileRouter.post(
+	"/upload/:folderId",
+	authMiddlewares.isAuthenticated,
+	fileUpload.array("fileUploads"),
+	fileValidators.uploadFiles,
+	fileMiddlewares.validateUploadFilesForm,
+	fileControllers.uploadFiles
 );
 
 module.exports = fileRouter;
