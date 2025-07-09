f1810734de42d27c632a91a28a13127a405c6979
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-10 07:05:36 +0800

    feat: Render the user's files

--- controllers/rootControllers.js
+++ controllers/rootControllers.js
@@ -1,14 +1,19 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel.js");
 
+// Load services
+const fileServices = require("../services/fileServices.js");
+
 async function handleRootRedirect(req, res, next) {
 	const username = req.user.username;
 	if (!username) {
 		// Render the pick username form if the user does not yet have a username
 		return res.status(200).redirect("/pick-username");
 	} else {
+		const files = await fileServices.getFiles({ id: req.user.id });
+
 		// Render the feed if the user already has a username
-		return res.status(200).render("home");
+		return res.status(200).render("home", { files: files });
 	}
 }
 
