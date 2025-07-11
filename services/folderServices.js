3a977549006a23803831418cb7b6a909beab7a2b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-12 03:39:42 +0800

    feat: Create service to get the user\s folders

---
+++ services/folderServices.js
@@ -0,0 +1,32 @@
+// Load path to resolve the absolute path to the .env folder (one level up from current directory)
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+// Load models
+const Folder = require("../models/folderModel.js");
+
+// Load services
+const storageServices = require("../services/storageServices.js");
+
+// Load utilities
+const date = require("../utils/date.js");
+
+async function getFolders({ id }) {
+	const folders = await Folder.findManyByOptions({ ownerId: id });
+
+	for (const folder of folders || []) {
+		// Attach profile picture url to the folder owner
+		folder.owner.profilePictureUrl = await storageServices.getFileUrl(
+			folder.owner.profilePictureFilename ||
+				process.env.DEFAULT_PROFILE_PICTURE_FILENAME
+		);
+
+		// Remove the time
+		folder.createdAt = date.removeTime(folder.createdAt);
+	}
+	return folders;
+}
+
+module.exports = {
+	getFolders,
+};
