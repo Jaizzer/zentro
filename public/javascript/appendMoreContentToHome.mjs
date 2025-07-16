ad69c18a6781caf9610c09798cc8a6a957effe97
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-07-16 09:08:56 +0800

    feat: Create module for rendering the folders and files in the home section

---
+++ public/javascript/appendMoreContentToHome.mjs
@@ -0,0 +1,32 @@
+import createFoldersSection from "/javascript/createFoldersSection.mjs";
+import createEmptyVaultMessage from "/javascript/createEmptyVaultMessage.mjs";
+import createFilesSection from "/javascript/createFilesSection.mjs";
+
+async function appendMoreContentToHome() {
+	const home = document.querySelector(".homeSection");
+
+	const filesSection = await createFilesSection({
+		initialUrl: "http://localhost:9000/file/recent",
+		nextUrl: "http://localhost:9000/file/recent?page=next",
+	});
+
+	const foldersSection = await createFoldersSection({
+		initialUrl: "http://localhost:9000/folder",
+		nextUrl: "http://localhost:9000/folder?page=next",
+	});
+
+	if (foldersSection) {
+		home.appendChild(foldersSection);
+	}
+
+	if (filesSection) {
+		home.appendChild(filesSection);
+	}
+
+	if (!filesSection && !foldersSection) {
+		const emptyVaultMessage = createEmptyVaultMessage();
+		home.appendChild(emptyVaultMessage);
+	}
+}
+
+appendMoreContentToHome();
