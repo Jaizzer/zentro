9a6a609251679bbcef8ba17253d21e3973200005
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-08-18 08:52:55 +0800

    ref: Move the logic for create fileDiv to a separate function

--- public/javascript/utils/createFiles.mjs
+++ public/javascript/utils/createFiles.mjs
@@ -10,118 +10,124 @@ export default function createFiles({ files, userId }) {
 	let filesHTML = [];
 
 	for (const file of files || []) {
-		// Create main file metadata container
-		const fileDiv = createElement({
-			tag: "div",
-			attributes: {
-				className: "file",
-			},
-		});
-		filesHTML.push(fileDiv);
-
-		// Create file label
-		const fileLabel = createElement({
-			tag: "div",
-			attributes: {
-				className: "label",
-			},
-		});
-		fileDiv.appendChild(fileLabel);
-
-		// Create file icon
-		const fileIconContainer = createElement({
-			tag: "span",
-			attributes: {
-				className: "iconContainer",
-				innerHTML: getFileIcon(file.type.split("/")[1]),
-			},
-		});
-		fileLabel.appendChild(fileIconContainer);
-
-		// Create filename
-		const filename = createElement({
-			tag: "span",
-			attributes: {
-				className: "name",
-				textContent: file.name,
-			},
-		});
-		fileLabel.appendChild(filename);
-
-		// Create file creation date
-		const creationDate = createElement({
-			tag: "time",
-			attributes: {
-				className: "creationDate",
-				dateTime: new Date(file.createdAt),
-				textContent: new Date(file.createdAt).toDateString(),
-			},
-		});
-		fileDiv.appendChild(creationDate);
-
-		// Create file owner information
-		const owner = createElement({
-			tag: "div",
-			attributes: {
-				className: "owner",
-			},
-		});
-		fileDiv.appendChild(owner);
-
-		const ownerProfilePicture = createElement({
-			tag: "img",
-			attributes: {
-				className: "profilePicture",
-				src: file.owner.profilePictureUrl,
-			},
-		});
-		owner.appendChild(ownerProfilePicture);
-
-		const ownerUsername = createElement({
-			tag: "span",
-			attributes: {
-				className: "ownerUsername",
-				textContent: `${
-					file.owner.id === userId ? "You" : file.owner.username
-				}`,
-			},
-		});
-		owner.appendChild(ownerUsername);
-
-		// Create file location information
-		const location = createElement({
-			tag: "div",
-			attributes: {
-				className: "location",
-			},
-		});
-		fileDiv.appendChild(location);
-
-		const folderIconContainer = createElement({
-			tag: "span",
-			attributes: {
-				className: "iconContainer",
-				innerHTML:
-					file.folder.name !== "Shared with you"
-						? defaultFolderIcon
-						: sharedWithYouFolderIcon,
-			},
-		});
-		location.appendChild(folderIconContainer);
-
-		const folderName = createElement({
-			div: "span",
-			attributes: {
-				className: "name",
-				textContent: file.folder.name,
-			},
-		});
-		location.appendChild(folderName);
-
-		// Create file actions
-		const actionList = createFileActionList(file);
-		fileDiv.appendChild(actionList);
+		const fileHTML = createFile({ file, userId });
+		filesHTML.push(fileHTML);
 	}
 
 	return filesHTML;
+}
+
+function createFile({ file, userId }) {
+	// Create main file metadata container
+	const fileDiv = createElement({
+		tag: "div",
+		attributes: {
+			className: "file",
+		},
+	});
+
+	// Create file label
+	const fileLabel = createElement({
+		tag: "div",
+		attributes: {
+			className: "label",
+		},
+	});
+	fileDiv.appendChild(fileLabel);
+
+	// Create file icon
+	const fileIconContainer = createElement({
+		tag: "span",
+		attributes: {
+			className: "iconContainer",
+			innerHTML: getFileIcon(file.type.split("/")[1]),
+		},
+	});
+	fileLabel.appendChild(fileIconContainer);
+
+	// Create filename
+	const filename = createElement({
+		tag: "span",
+		attributes: {
+			className: "name",
+			textContent: file.name,
+		},
+	});
+	fileLabel.appendChild(filename);
+
+	// Create file creation date
+	const creationDate = createElement({
+		tag: "time",
+		attributes: {
+			className: "creationDate",
+			dateTime: new Date(file.createdAt),
+			textContent: new Date(file.createdAt).toDateString(),
+		},
+	});
+	fileDiv.appendChild(creationDate);
+
+	// Create file owner information
+	const owner = createElement({
+		tag: "div",
+		attributes: {
+			className: "owner",
+		},
+	});
+	fileDiv.appendChild(owner);
+
+	const ownerProfilePicture = createElement({
+		tag: "img",
+		attributes: {
+			className: "profilePicture",
+			src: file.owner.profilePictureUrl,
+		},
+	});
+	owner.appendChild(ownerProfilePicture);
+
+	const ownerUsername = createElement({
+		tag: "span",
+		attributes: {
+			className: "ownerUsername",
+			textContent: `${
+				file.owner.id === userId ? "You" : file.owner.username
+			}`,
+		},
+	});
+	owner.appendChild(ownerUsername);
+
+	// Create file location information
+	const location = createElement({
+		tag: "div",
+		attributes: {
+			className: "location",
+		},
+	});
+	fileDiv.appendChild(location);
+
+	const folderIconContainer = createElement({
+		tag: "span",
+		attributes: {
+			className: "iconContainer",
+			innerHTML:
+				file.folder.name !== "Shared with you"
+					? defaultFolderIcon
+					: sharedWithYouFolderIcon,
+		},
+	});
+	location.appendChild(folderIconContainer);
+
+	const folderName = createElement({
+		div: "span",
+		attributes: {
+			className: "name",
+			textContent: file.folder.name,
+		},
+	});
+	location.appendChild(folderName);
+
+	// Create file actions
+	const actionList = createFileActionList(file);
+	fileDiv.appendChild(actionList);
+
+	return fileDiv;
 }
