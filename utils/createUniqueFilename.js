2b89a7c70d977ecf77afdba06a975cffcd30a6bb
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-08-26 10:28:54 +0800

    feat: Create utility function that creates a unique file name base on the existing similar file names

---
+++ utils/createUniqueFilename.js
@@ -0,0 +1,21 @@
+async function createUniqueFilename({ currentFileNames }) {
+	// Sort the file names alphanumerically
+	const sortedFileNames = currentFileNames.sort((a, b) => a > b);
+
+	// Get the last file name
+	const last = sortedFileNames[sortedFileNames.length - 1];
+
+	// Split the file name into its different parts
+	const [fileName, extension] = last.split(".");
+	const [actualName, index] = fileName.split(" - ");
+
+	if (index) {
+		// Return the file name with with an index up by 1 from the current last file name
+		return `${actualName} - ${parseInt(index) + 1}.${extension}`;
+	} else {
+		// Return a file name win an index 1
+		return `${actualName} - 1.${extension}`;
+	}
+}
+
+module.exports = createUniqueFilename;
