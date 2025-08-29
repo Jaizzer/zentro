8e4bc4f60e5a68a76091725f7138701099840519
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-08-29 15:27:21 +0800

    doc: Update comments

--- utils/createUniqueFilename.js
+++ utils/createUniqueFilename.js
@@ -1,11 +1,11 @@
-async function createUniqueFilename({ currentFileNames }) {
+function createUniqueFilename({ currentFileNames }) {
 	// Sort the file names alphanumerically
 	const sortedFileNames = currentFileNames.sort((a, b) => a > b);
 
 	// Get the last file name
 	const last = sortedFileNames[sortedFileNames.length - 1];
 
-	// Split the file name into its different parts
+	// Split the file name into its multiple parts
 	const [fileName, extension] = last.split(".");
 	const [actualName, index] = fileName.split(" - ");
 
@@ -13,7 +13,7 @@ async function createUniqueFilename({ currentFileNames }) {
 		// Return the file name with with an index up by 1 from the current last file name
 		return `${actualName} - ${parseInt(index) + 1}.${extension}`;
 	} else {
-		// Return a file name win an index 1
+		// Return a file name with an index 1
 		return `${actualName} - 1.${extension}`;
 	}
 }
