8180d56ad8cd0deb5cc1c7ab8b22c048f20ae1d4
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-01 15:18:14 +0800

    feat: Submit files via the fetch api

--- public/javascript/upload.js
+++ public/javascript/upload.js
@@ -1,12 +1,28 @@
 const uploadFilesForm = document.querySelector(".uploadFilesForm");
 const uploadButton = document.querySelector(".uploadButton");
 
+// Access the file input
+const fileUploadsInput = document.querySelector(`#fileUploads`);
+uploadFilesForm.addEventListener("submit", (e) => {
+	// prevent default form submission
+	e.preventDefault();
+
+	const formData = new FormData(uploadFilesForm);
+	fetch("http://localhost:9000/file/upload", {
+		method: "POST",
+		body: formData,
+	})
+		.then((res) => console.log(res))
+		.catch((err) => console.error(err));
+});
+
 if (uploadFilesForm && uploadButton) {
 	uploadButton.addEventListener("click", () => {
 		// Disable button prevent resubmissions
 		uploadButton.disabled = true;
 
 		// Submit the form
-		uploadFilesForm.submit();
+		const submitEvent = new Event("submit");
+		uploadFilesForm.dispatchEvent(submitEvent);
 	});
 }
