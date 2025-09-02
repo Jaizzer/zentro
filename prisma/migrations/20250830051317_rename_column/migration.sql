a631cead004c36590984c9bedc1b7dc6fb405339
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-03 05:01:40 +0800

    chore: Resolve database drifts

--- prisma/migrations/20250830051317_rename_column/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isRoot` on the `folders` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "folders" DROP COLUMN "isRoot",
-ADD COLUMN     "is_root" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20250902210104_resolve_drifts/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "folders" ADD COLUMN     "is_root" BOOLEAN NOT NULL DEFAULT false;
