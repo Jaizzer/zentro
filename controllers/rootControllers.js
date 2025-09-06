e451dfee5978eed24674fcb7a0d810bbacfbf3cb
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-06 11:30:03 +0800

    feat: Remove unsused file and folder retrieval since the frontend is now the one requesting for the folder and files via fetch

--- controllers/rootControllers.js
+++ controllers/rootControllers.js
@@ -1,23 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel.js");
 
-// Load services
-const fileServices = require("../services/fileServices.js");
-const folderServices = require("../services/folderServices.js");
-
 async function handleRootRedirect(req, res, next) {
 	const username = req.user.username;
 	if (!username) {
 		// Render the pick username form if the user does not yet have a username
 		return res.status(200).redirect("/pick-username");
 	} else {
-		// Retrieve the user's folders
-		const folders = await folderServices.get({ id: req.user.id });
-
 		// Render the feed if the user already has a username
-		return res
-			.status(200)
-			.render("home", { files: null, folders: folders });
+		return res.status(200).render("home");
 	}
 }
 
