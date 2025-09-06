8c561a253f36570657f9b99b05f2a6c4d68f633f
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-06 14:16:27 +0800

    feat: Render the folder contents when accessed

--- public/javascript/renderFolder.mjs
+++ public/javascript/renderFolder.mjs
@@ -1,7 +1,26 @@
+import createFoldersSection from "./createFoldersSection.mjs";
+import createFilesSection from "./createFilesSection.mjs";
+
 async function renderFolder() {
+	// Access the main folder container
 	const folderContainer = document.querySelector(".folderSection");
-	const folder = JSON.parse(folderContainer.dataset.folder);
-	console.log(folder);
+
+	// Retrieve the injected contents of the folder
+	const folder = JSON.parse(folderContainer.dataset.folder)[0];
+
+	// Render the children folders
+	if (folder.childrenFolders.length) {
+		const folderSection = await createFoldersSection({
+			folders: folder.childrenFolders,
+		});
+		folderContainer.appendChild(folderSection);
+	}
+
+	// Render children files
+	if (folder.files.length) {
+		const fileSection = await createFilesSection({ files: folder.files });
+		folderContainer.appendChild(fileSection);
+	}
 }
 
 renderFolder();
