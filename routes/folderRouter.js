31c07ccde64d637080c1023d988822cf73a461bd
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-06 11:50:44 +0800

    feat: Create separate route for actual folder retrieval

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -4,9 +4,15 @@ const folderControllers = require("../controllers/folderControllers.js");
 const authMiddlewares = require("../middlewares/authMiddlewares.js");
 
 folderRouter.get(
-	"/",
+	"/:folderId",
 	authMiddlewares.isAuthenticated,
 	folderControllers.renderInitialFolderPage
+);
+
+folderRouter.get(
+	"/retrieve/:folderId",
+	authMiddlewares.isAuthenticated,
+	folderControllers.getFolders
 );
 
 module.exports = folderRouter;
