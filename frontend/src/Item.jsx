1941e7eabfb626c3088208553b446aaf87b89334
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-09 11:30:45 +0800

    ref: Create a general Item component that could support both files and folder

--- frontend/src/Item.jsx
+++ frontend/src/Item.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
-export default function File({
-	file,
+export default function Item({
+	item,
 	isInSelectMode,
-	selectFileCallback,
-	unselectFileCallback,
+	selectItemCallback,
+	unselectItemCallback,
 }) {
 	const [isSelected, setIsSelected] = useState(false);
 
-	// Render a loading message if the file is not yet available
-	if (!file) {
+	// Render a loading message if the item is not yet available
+	if (!item) {
 		return <div>Loading...</div>;
 	}
 
 	// Create a checkbox
 	let checkbox;
-	let handleFileSelect = (e) => {
+	let handleItemSelect = (e) => {
 		// Avoid triggering the parent element's event/s
 		e.stopPropagation();
 
-		// Execute the call back for selecting and unselecting the file
+		// Execute the call back for selecting and unselecting the item
 		if (!isSelected) {
 			setIsSelected(!isSelected);
-			selectFileCallback();
+			selectItemCallback();
 		} else {
 			setIsSelected(!isSelected);
-			unselectFileCallback();
+			unselectItemCallback();
 		}
 	};
 
@@ -35,7 +35,7 @@ export default function File({
 				className="checkbox"
 				type="checkbox"
 				checked={isSelected}
-				onChange={handleFileSelect}
+				onChange={handleItemSelect}
 			/>
 		);
 	}
@@ -45,18 +45,18 @@ export default function File({
 			{checkbox}
 			<div className="label">
 				<span className="iconContainer"></span>
-				<span className="name">{file.name}</span>
+				<span className="name">{item.name}</span>
 			</div>
-			<time className="creationDate" dateTime={new Date(file.createdAt)}>
-				{new Date(file.createdAt).toDateString()}
+			<time className="creationDate" dateTime={new Date(item.createdAt)}>
+				{new Date(item.createdAt).toDateString()}
 			</time>
 			<div className="owner">
-				<img src={file.owner.profilePictureUrl} />
-				<span className="username">{file.owner.username}</span>
+				<img src={item.owner.profilePictureUrl} />
+				<span className="username">{item.owner.username}</span>
 			</div>
 			<div className="location">
 				<span className="iconContainer"></span>
-				<div className="name">{file.folder.name}</div>
+				<div className="name">{item.folder.name}</div>
 			</div>
 			<div className="actions"></div>
 		</a>
